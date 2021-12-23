16a8f8b22fa5e4998252ca8feeaf949acfef8d98
Rudy Bourez <bourez.rudy@gmail.com>
2021-12-23 16:56:59 +0100

    covariance|corrélation

--- distribution_normale.py
+++ distribution_normale.py
@@ -1,9 +1,6 @@
-from scipy.integrate import quad
-
 import matplotlib.pyplot as plt
 import scipy.stats
 import numpy as np
-
 
 x_min = 0.0
 x_max = 16.0
@@ -12,18 +9,15 @@ mean = 8.0
 std = 2.0
 
 x = np.linspace(x_min, x_max, 100)
-
 y = scipy.stats.norm.pdf(x,mean,std)
 
 plt.plot(x,y, color='coral')
-
 plt.grid()
 
 plt.xlim(x_min,x_max)
 plt.ylim(0,0.25)
 
 plt.title('How to plot a normal distribution in python with matplotlib',fontsize=10)
-
 plt.xlabel('x')
 plt.ylabel('Normal Distribution')
 
